ebfa6592ceb13a48acb7d38778724ca814fb56c7
codeWithNithin <nithinkottary25@gmail.com>
2025-08-20 22:03:56 +0530

    add api endpoints

--- client/customer-login/src/http/api.ts
+++ client/customer-login/src/http/api.ts
@@ -4,3 +4,10 @@ import api from "./client";
 
 export const login = (credentials: Credentials) =>
     api.post(`/auth/login`, credentials);
+
+export const register = (credentials: Credentials) =>
+    api.post(`/auth/register`, credentials);
+
+export const self = () => api.get(`/auth/self`);
+
+export const logout = () => api.post(`/auth/logout`);
