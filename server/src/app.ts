db0b7fb27aea21cf391bd7242e854b3ec34e912d
codeWithNithin <nithinkottary25@gmail.com>
2025-08-20 16:54:04 +0530

    add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,31 @@
 import express from "express";
-import cookieParser from "cookie-parser" 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import healthCheckRouter from "./healthCheck/healthCheck.routes.js";
 import { globalErrHandler } from "./common/middlewares/globarlErrorHandler.js";
 import authRouter from "./users/auth.router.js";
 import userRouter from "./users/user.router.js";
+import { Config } from "./config/index.js";
 
 const app = express();
 
-app.use(cookieParser())
+const ALLOWED_DOMAINS = [Config.CLIENT_UI];
+
+app.use(
+  cors({
+    origin: ALLOWED_DOMAINS as string[],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/health-check", healthCheckRouter);
-app.use('/auth', authRouter);
-app.use('/users', userRouter)
+app.use("/auth", authRouter);
+app.use("/users", userRouter);
 
-
-app.use(globalErrHandler)
+app.use(globalErrHandler);
 
 export default app;
--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: `./.env.${process.env.NODE_ENV || "development"}` });
 
-const { PORT, NODE_ENV, JWT_SECRET, MONGO_URI } = process.env;
+const { PORT, NODE_ENV, JWT_SECRET, MONGO_URI, CLIENT_UI } = process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
   JWT_SECRET,
   MONGO_URI,
+  CLIENT_UI
 };
